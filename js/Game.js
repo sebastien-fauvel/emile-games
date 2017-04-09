8fcba147288fb4336ccbcdfc82083d9a53c9581f
emile <emile.fauvel@gmail.com>
2017-04-09 13:47:19 +0200

    Implemented doors

--- js/Game.js
+++ js/Game.js
@@ -1,119 +1,136 @@
 var TopDownGame = TopDownGame || {};
 
 //title screen
-TopDownGame.Game = function(){};
+TopDownGame.Game = function () {
+};
 
 TopDownGame.Game.prototype = {
-  create: function() {
-    this.map = this.game.add.tilemap('level1');
+    loadMap: function (tilemap, playerStart) {
+        this.map = this.game.add.tilemap(tilemap);
 
-    //the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
-    this.map.addTilesetImage('tiles', 'gameTiles');
+        //the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
+        this.map.addTilesetImage('images-du-monde', 'images-du-monde', 16, 16, 1, 1);
+        this.map.addTilesetImage('images-dans-la-maison', 'images-dans-la-maison', 16, 16, 1, 1);
 
-    //create layer
-    this.backgroundlayer = this.map.createLayer('backgroundLayer');
-    this.blockedLayer = this.map.createLayer('blockedLayer');
+        //create layer
+        this.backgroundlayer = this.map.createLayer('backgroundLayer');
+        this.blockedLayer = this.map.createLayer('blockedLayer');
 
-    //collision on blockedLayer
-    this.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
+        //collision on blockedLayer
+        this.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
 
-    //resizes the game world to match the layer dimensions
-    this.backgroundlayer.resizeWorld();
+        //resizes the game world to match the layer dimensions
+        this.backgroundlayer.resizeWorld();
 
-    this.createItems();
-    this.createDoors();    
+        this.createItems();
+        this.createDoors();
 
-    //create player
-    var result = this.findObjectsByType('playerStart', this.map, 'objectsLayer')
-    this.player = this.game.add.sprite(result[0].x, result[0].y, 'player');
-    this.game.physics.arcade.enable(this.player);
+        //create player
+        if ('undefined' === typeof playerStart) {
+            var result = this.findObjectsByType('playerStart', this.map, 'objectsLayer');
+            playerStart = {x: result[0].x, y: result[0].y};
+        }
+        this.player = this.game.add.sprite(playerStart.x, playerStart.y, 'player');
+        this.game.physics.arcade.enable(this.player);
+        this.game.camera.follow(this.player);
+    },
+    create: function () {
+        this.loadMap('wiese');
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    },
+    createItems: function () {
+        //create items
+        this.items = this.game.add.group();
+        this.items.enableBody = true;
+        var item;
+        result = this.findObjectsByType('item', this.map, 'objectsLayer');
+        result.forEach(function (element) {
+            this.createFromTiledObject(element, this.items);
+        }, this);
+    },
+    createDoors: function () {
+        //create doors
+        this.doors = this.game.add.group();
+        this.doors.enableBody = true;
+        result = this.findObjectsByType('door', this.map, 'objectsLayer');
 
-    //the camera will follow the player in the world
-    this.game.camera.follow(this.player);
+        result.forEach(function (element) {
+            this.createFromTiledObject(element, this.doors);
+        }, this);
+    },
 
-    //move player with cursor keys
-    this.cursors = this.game.input.keyboard.createCursorKeys();
+    //find objects in a Tiled layer that containt a property called "type" equal to a certain value
+    findObjectsByType: function (type, map, layer) {
+        var result = new Array();
+        map.objects[layer].forEach(function (element) {
+            if (element.properties.type === type) {
+                //Phaser uses top left, Tiled bottom left so we have to adjust
+                //also keep in mind that the cup images are a bit smaller than the tile which is 16x16
+                //so they might not be placed in the exact position as in Tiled
+                element.y -= map.tileHeight;
+                result.push(element);
+            }
+        });
+        return result;
+    },
+    //create a sprite from an object
+    createFromTiledObject: function (element, group) {
+        var sprite;
+        if ('undefined' !== typeof element.properties.sprite) {
+            sprite = group.create(element.x, element.y, element.properties.sprite);
+        } else {
+            sprite = group.create(element.x, element.y + this.map.tileHeight, '16x16');
+        }
 
-  },
-  createItems: function() {
-    //create items
-    this.items = this.game.add.group();
-    this.items.enableBody = true;
-    var item;    
-    result = this.findObjectsByType('item', this.map, 'objectsLayer');
-    result.forEach(function(element){
-      this.createFromTiledObject(element, this.items);
-    }, this);
-  },
-  createDoors: function() {
-    //create doors
-    this.doors = this.game.add.group();
-    this.doors.enableBody = true;
-    result = this.findObjectsByType('door', this.map, 'objectsLayer');
+        //copy all properties to the sprite
+        Object.keys(element.properties).forEach(function (key) {
+            sprite[key] = element.properties[key];
+        });
+    },
+    update: function () {
+        if (!this.player) {
+            return;
+        }
+        //collision
+        this.game.physics.arcade.collide(this.player, this.blockedLayer);
+        this.game.physics.arcade.overlap(this.player, this.items, this.collect, null, this);
+        this.game.physics.arcade.overlap(this.player, this.doors, this.enterDoor, null, this);
 
-    result.forEach(function(element){
-      this.createFromTiledObject(element, this.doors);
-    }, this);
-  },
+        //player movement
 
-  //find objects in a Tiled layer that containt a property called "type" equal to a certain value
-  findObjectsByType: function(type, map, layer) {
-    var result = new Array();
-    map.objects[layer].forEach(function(element){
-      if(element.properties.type === type) {
-        //Phaser uses top left, Tiled bottom left so we have to adjust
-        //also keep in mind that the cup images are a bit smaller than the tile which is 16x16
-        //so they might not be placed in the exact position as in Tiled
-        element.y -= map.tileHeight;
-        result.push(element);
-      }      
-    });
-    return result;
-  },
-  //create a sprite from an object
-  createFromTiledObject: function(element, group) {
-    var sprite = group.create(element.x, element.y, element.properties.sprite);
+        this.player.body.velocity.x = 0;
 
-      //copy all properties to the sprite
-      Object.keys(element.properties).forEach(function(key){
-        sprite[key] = element.properties[key];
-      });
-  },
-  update: function() {
-    //collision
-    this.game.physics.arcade.collide(this.player, this.blockedLayer);
-    this.game.physics.arcade.overlap(this.player, this.items, this.collect, null, this);
-    this.game.physics.arcade.overlap(this.player, this.doors, this.enterDoor, null, this);
+        if (this.cursors.up.isDown) {
+            if (this.player.body.velocity.y == 0)
+                this.player.body.velocity.y -= 50;
+        }
+        else if (this.cursors.down.isDown) {
+            if (this.player.body.velocity.y == 0)
+                this.player.body.velocity.y += 50;
+        }
+        else {
+            this.player.body.velocity.y = 0;
+        }
+        if (this.cursors.left.isDown) {
+            this.player.body.velocity.x -= 50;
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.body.velocity.x += 50;
+        }
+    },
+    collect: function (player, collectable) {
+        console.log('yummy!');
 
-    //player movement
-    
-    this.player.body.velocity.x = 0;
-
-    if(this.cursors.up.isDown) {
-      if(this.player.body.velocity.y == 0)
-      this.player.body.velocity.y -= 50;
+        //remove sprite
+        collectable.destroy();
+    },
+    enterDoor: function (player, door) {
+        this.map.removeAllLayers();
+        this.backgroundlayer.destroy();
+        this.blockedLayer.destroy();
+        this.items.destroy();
+        this.doors.destroy();
+        this.player.destroy();
+        this.loadMap(door.targetTilemap, {x: door.targetX, y: door.targetY});
     }
-    else if(this.cursors.down.isDown) {
-      if(this.player.body.velocity.y == 0)
-      this.player.body.velocity.y += 50;
-    }
-    else {
-      this.player.body.velocity.y = 0;
-    }
-    if(this.cursors.left.isDown) {
-      this.player.body.velocity.x -= 50;
-    }
-    else if(this.cursors.right.isDown) {
-      this.player.body.velocity.x += 50;
-    }
-  },
-  collect: function(player, collectable) {
-    console.log('yummy!');
-
-    //remove sprite
-    collectable.destroy();
-  },
-  enterDoor: function(player, door) {
-    console.log('entering door that will take you to '+door.targetTilemap+' on x:'+door.targetX+' and y:'+door.targetY);
-  },
 };
--- js/Preload.js
+++ js/Preload.js
@@ -1,26 +1,32 @@
 var TopDownGame = TopDownGame || {};
 
 //loading the game assets
-TopDownGame.Preload = function(){};
+TopDownGame.Preload = function () {
+};
 
 TopDownGame.Preload.prototype = {
-  preload: function() {
-    //show loading screen
-    this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'preloadbar');
-    this.preloadBar.anchor.setTo(0.5);
+    preload: function () {
+        //show loading screen
+        this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'preloadbar');
+        this.preloadBar.anchor.setTo(0.5);
 
-    this.load.setPreloadSprite(this.preloadBar);
+        this.load.setPreloadSprite(this.preloadBar);
 
-    //load game assets
-    this.load.tilemap('level1', 'assets/tilemaps/level1.json', null, Phaser.Tilemap.TILED_JSON);
-    this.load.image('gameTiles', 'assets/images/tiles.png');
-    this.load.image('greencup', 'assets/images/greencup.png');
-    this.load.image('bluecup', 'assets/images/bluecup.png');
-    this.load.image('player', 'assets/images/player.png');
-    this.load.image('browndoor', 'assets/images/browndoor.png');
-    
-  },
-  create: function() {
-    this.state.start('Game');
-  }
+        //load game assets
+        this.load.tilemap('wiese', 'assets/tilemaps/wiese.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.tilemap('haus', 'assets/tilemaps/haus.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.image('images-du-monde', 'assets/images/images-du-monde.png');
+        this.load.image('images-dans-la-maison', 'assets/images/images-dans-la-maison.gif');
+        this.load.image('16x16', 'assets/images/16x16.gif');
+        this.load.tilemap('level1', 'assets/tilemaps/level1.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.image('gameTiles', 'assets/images/tiles.png');
+        this.load.image('greencup', 'assets/images/greencup.png');
+        this.load.image('bluecup', 'assets/images/bluecup.png');
+        this.load.image('player', 'assets/images/player.png');
+        this.load.image('browndoor', 'assets/images/browndoor.png');
+
+    },
+    create: function () {
+        this.state.start('Game');
+    }
 };
